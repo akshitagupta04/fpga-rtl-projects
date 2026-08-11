8fcc1ca1c9e6eb55cc106319df71e96d28c1fbc7
Akshita Gupta <74429600+akshitagupta04@users.noreply.github.com>
2026-08-11 05:35:06 +0530

    NOR logic gate VHDL Testbench

---
+++ Learning-Lab/VHDL-Projects/NOR-Logic-Gate/simulation/tb_nor3.vhd
@@ -0,0 +1,59 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity tb_nor3 is
+--empty
+end tb_nor3;
+
+architecture rtl of tb_nor3 is
+--Component declaration for Unit Under Test (UUT)
+component nor3
+port(
+A : in std_logic;
+B : in std_logic;
+Y : out std_logic
+);
+end component;
+
+--internal signal declaration
+signal A,B : std_logic;
+signal Y : std_logic;
+
+begin
+--instantiate the unit under test
+nor3_port: nor3 port map(
+A => A,
+B => B,
+Y => Y
+);
+
+tb: process
+begin
+
+--test case 1
+A <= '0';
+B <= '0';
+wait for 100ns;
+
+--test case 2
+A <= '0';
+B <= '1';
+wait for 100ns;
+
+--test case 3
+A <= '1';
+B <= '0';
+wait for 100ns;
+
+--test case 4
+A <= '1';
+B <= '1';
+wait for 100ns;
+--terminate simulation
+assert false;
+    report "Simulation Completed"
+severity failure;
+end process tb;
+end rtl;
